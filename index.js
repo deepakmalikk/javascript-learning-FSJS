8e3c666c522d0a1398731bb32b33620767a68a16
deepakmalikk <deepak164malik@gmail.com>
2023-01-21 21:49:46 +0530

    added Primitive Data Types

---
+++ index.js
@@ -0,0 +1,23 @@
+// All Data Types Primitive Data Types And Non-Primitive(object)
+ 
+
+// Primitive Data Types  that can store only one value
+
+
+// 1. number 
+
+var a = 8;
+
+// 2. string
+
+var user_Name = 'deepak'
+
+// 3. Boolean
+ 
+var isLoggedIn = true
+
+// 4. empty value data type
+
+null
+undefined
+
