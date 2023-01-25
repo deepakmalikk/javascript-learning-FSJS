473660d869b44ea2f3f38ea2144e66a320f559f6
deepakmalikk <deepak164malik@gmail.com>
2023-01-25 17:38:57 +0530

    updated math property and methods in this file

---
+++ script3.js
@@ -0,0 +1,44 @@
+//  Math property
+
+let pi = Math.PI
+
+console.log(pi)
+// 3.141592653589793
+
+// Math Methods
+
+console.log(Math.round(pi))
+// 3
+
+console.log(Math.ceil(pi))
+// 4
+
+console.log(Math.floor(pi))
+// 3
+
+console.log(Math.sqrt(4))
+// 2
+
+console.log(Math.abs(-4))
+// 4
+
+console.log(Math.random())
+// 0.78109104228986
+
+console.log(Math.min(3,5,9))
+// 3
+
+console.log(Math.max(3,5,9))
+// 9
+
+console.log(Math.LN2)
+// 0.6931471805599453
+
+console.log(Math.pow(3,2))
+//9
+
+console.log(Math.sin(60))
+// -0.3048106211022167
+
+console.log(Math.LN10)
+// 2.302585092994046
