755678d27c95e6544322f3de5463eb8304f3a201
deepakmalikk <deepak164malik@gmail.com>
2023-01-25 14:26:35 +0530

    modified the format of the file

--- script2.js
+++ script2.js
@@ -1,13 +1,13 @@
 // Array methods 
 
-const arr = [1, 2 , 'deepak', 'malik', true];
+const arr = [1, 2, 'deepak', 'malik', true];
 
 console.log(arr.length)
 // 5
 
 // to get last element of array
 
-console.log(arr[arr.length-1])
+console.log(arr[arr.length - 1])
 //  true
 
 console.log(arr.push(5))
@@ -24,14 +24,14 @@ console.log(arr)
 
 
 
-let arr2 =[false, 9, 8, 7, 6]
-let arr3 =[5, 4, 3, 2, 1]
+let arr2 = [false, 9, 8, 7, 6]
+let arr3 = [5, 4, 3, 2, 1]
 
 console.log(arr.concat(arr2))
 // [ 1, 2, 'deepak', 'malik', true, false, 9, 8, 7, 6 ]
 console.log(arr)
 // [ 1, 2, 'deepak', 'malik', true ]
-console.log(arr.concat(arr2,arr3))   //multiple array concatenation
+console.log(arr.concat(arr2, arr3))   //multiple array concatenation
 /*[
   1,       2,    'deepak',
   'malik', true, false,
@@ -41,15 +41,15 @@ console.log(arr.concat(arr2,arr3))   //multiple array concatenation
 ]*/
 
 
-console.log(arr.slice(3,5))
+console.log(arr.slice(3, 5))
 // [ 'malik', true ]
 
 
-console.log(arr.splice(2, 'deepu','singh'))
+console.log(arr.splice(2, 'deepu', 'singh'))
 // []  removed elements
 console.log(arr)
 // [ 1, 2, 'singh', 'deepak', 'malik', true ]  new array
-console.log(arr.splice(2,2,'hello','bye'))
+console.log(arr.splice(2, 2, 'hello', 'bye'))
 // [ 'singh', 'deepak' ]  removed elements
 console.log(arr)
 // [ 1, 2, 'hello', 'bye', 'malik', true ]     new array
@@ -83,7 +83,7 @@ console.log(newarr.includes('3'))
 console.log(newarr.includes(3))
 //  true
 
-console.log(newarr.copyWithin(2,4,6))
+console.log(newarr.copyWithin(2, 4, 6))
 /* [
   -1, 1, 4, 5,
    4, 5, 6
