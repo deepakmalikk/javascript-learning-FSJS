2db74e351699909008dd430dfbf72a17e994e588
deepakmalikk <deepak164malik@gmail.com>
2023-02-02 23:26:33 +0530

    for in loop for object

--- script5.js
+++ script5.js
@@ -1,18 +1,29 @@
 // object :-
 
-let obj ={
-    name: 'deepak',
-    lastName: 'malik',
-    user: 'admin',
-    // methods on object
-    call: function (){
-        return `let's call ${this.name}`
-    }
+// let obj ={
+//     name: 'deepak',
+//     lastName: 'malik',
+//     user: 'admin',
+//     // methods on object
+//     call: function (){
+//         return `let's call ${this.name}`
+//     }
+// }
+
+// console.log(obj)
+
+// console.log(obj['lastName'])
+// console.table(obj)
+// console.log(Object.keys(obj))
+// console.log(Object.values(obj))
+
+
+let user= {
+    productName: 'Laptop',
+    prodID: '12345679',
+    prodValue: '2450$'
 }
 
-console.log(obj)
-
-console.log(obj['lastName'])
-console.table(obj)
-console.log(Object.keys(obj))
-console.log(Object.values(obj))
+for(let values in user){
+    console.log(`key is ${values} and value is ${user[values]}`)
+}
