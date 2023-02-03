5abda292ca618a183836b713c950bbb57cfbcc2f
deepakmalikk <deepak164malik@gmail.com>
2023-02-03 23:22:01 +0530

    added one syntax of setTimeout()

--- script5.js
+++ script5.js
@@ -1,75 +1,87 @@
-// object :-
+// // object :-
 
-let obj = {
-    name: 'deepak',
-    lastName: 'malik',
-    user: 'admin',
-    // methods on object
-    call: function () {
-        return `let's call ${this.name}`
-    }
-}
+// let obj = {
+//     name: 'deepak',
+//     lastName: 'malik',
+//     user: 'admin',
+//     // methods on object
+//     call: function () {
+//         return `let's call ${this.name}`
+//     }
+// }
 
-console.log(obj)
-/* {
-  name: 'deepak',
-  lastName: 'malik',
-  user: 'admin',
-  call: [Function: call]
-}*/
+// console.log(obj)
+// /* {
+//   name: 'deepak',
+//   lastName: 'malik',
+//   user: 'admin',
+//   call: [Function: call]
+// }*/
 
-console.log(obj['lastName'])
-// malik
+// console.log(obj['lastName'])
+// // malik
 
-console.table(obj)
-/*
-┌──────────┬──────────┐
-│ (index)  │  Values  │
-├──────────┼──────────┤
-│   name   │ 'deepak' │
-│ lastName │ 'malik'  │
-│   user   │ 'admin'  │
-│   call   │          │
-└──────────┴──────────┘
-*/
+// console.table(obj)
+// /*
+// ┌──────────┬──────────┐
+// │ (index)  │  Values  │
+// ├──────────┼──────────┤
+// │   name   │ 'deepak' │
+// │ lastName │ 'malik'  │
+// │   user   │ 'admin'  │
+// │   call   │          │
+// └──────────┴──────────┘
+// */
 
-console.log(Object.keys(obj))
-// [ 'name', 'lastName', 'user', 'call' ]
+// console.log(Object.keys(obj))
+// // [ 'name', 'lastName', 'user', 'call' ]
 
-console.log(Object.values(obj))
-// [ 'deepak', 'malik', 'admin', [Function: call] ]
-
-
-let user = {
-    productName: 'Laptop',
-    prodID: '12345679',
-    prodValue: '2450$'
-}
-
-for (let values in user) {
-    console.log(`key is ${values} and value is ${user[values]}`)
-}
-
-/*
-key is productName and value is Laptop
-key is prodID and value is 12345679
-key is prodValue and value is 2450$
- */
+// console.log(Object.values(obj))
+// // [ 'deepak', 'malik', 'admin', [Function: call] ]
 
 
+// let user = {
+//     productName: 'Laptop',
+//     prodID: '12345679',
+//     prodValue: '2450$'
+// }
 
-// HOF
+// for (let values in user) {
+//     console.log(`key is ${values} and value is ${user[values]}`)
+// }
+
+// /*
+// key is productName and value is Laptop
+// key is prodID and value is 12345679
+// key is prodValue and value is 2450$
+//  */
 
 
-// callback function
-function callback(n) {
-    return 2 + n;
-}
 
-let newfun = (callback, x) => {
-    return callback(x) + x
-}
+// // HOF
 
-console.log(newfun(callback, 3))
+
+// // 1. callback function
+// function callback(n) {
+//     return 2 + n;
+// }
+
+// let newfun = (callback, x) => {
+//     return callback(x) + x
+// }
+
+// console.log(newfun(callback, 3))
 
 // 8
+
+//  2. setTimeout(function,count)
+
+// eg1:
+function hello(){
+    console.log("Hello From javascript");
+}
+
+setTimeout(hello,3000)
+
+// After 3 second o/p 
+// Hello From javascript
