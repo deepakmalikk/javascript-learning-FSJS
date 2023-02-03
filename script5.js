d4746227500b24b17cd7691c2d5d0fdf6f0ce220
deepakmalikk <deepak164malik@gmail.com>
2023-02-03 23:17:19 +0530

    added output

--- script5.js
+++ script5.js
@@ -1,21 +1,43 @@
 // object :-
 
-// let obj ={
-//     name: 'deepak',
-//     lastName: 'malik',
-//     user: 'admin',
-//     // methods on object
-//     call: function (){
-//         return `let's call ${this.name}`
-//     }
-// }
+let obj ={
+    name: 'deepak',
+    lastName: 'malik',
+    user: 'admin',
+    // methods on object
+    call: function (){
+        return `let's call ${this.name}`
+    }
+}
 
-// console.log(obj)
+console.log(obj)
+/* {
+  name: 'deepak',
+  lastName: 'malik',
+  user: 'admin',
+  call: [Function: call]
+}*/
 
-// console.log(obj['lastName'])
-// console.table(obj)
-// console.log(Object.keys(obj))
-// console.log(Object.values(obj))
+console.log(obj['lastName'])
+// malik
+
+console.table(obj)
+/*
+┌──────────┬──────────┐
+│ (index)  │  Values  │
+├──────────┼──────────┤
+│   name   │ 'deepak' │
+│ lastName │ 'malik'  │
+│   user   │ 'admin'  │
+│   call   │          │
+└──────────┴──────────┘
+*/
+
+console.log(Object.keys(obj))
+// [ 'name', 'lastName', 'user', 'call' ]
+
+console.log(Object.values(obj))
+// [ 'deepak', 'malik', 'admin', [Function: call] ]
 
 
 let user= {
@@ -28,8 +50,18 @@ for(let values in user){
     console.log(`key is ${values} and value is ${user[values]}`)
 }
 
+/*
+key is productName and value is Laptop
+key is prodID and value is 12345679
+key is prodValue and value is 2450$
+ */
+
+
+
 // HOF
 
+
+// callback function
 function callback (n){
     return 2+n;
 }
@@ -39,3 +71,5 @@ let newfun =(callback,x)=>{
 }
 
 console.log(newfun(callback,3))
+
+// 8
