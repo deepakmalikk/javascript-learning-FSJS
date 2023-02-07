0e0bba9d0653368c1fb757a66ec1e9c1702346c6
deepakmalikk <deepak164malik@gmail.com>
2023-02-07 13:07:28 +0530

    solved 1st problem

---
+++ javaScriptQue/variable.js
@@ -0,0 +1,17 @@
+// 1. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with there type.
+
+var variable1 = "learnCodeOnline";
+var variable2 = undefined;
+var variable3 = null;
+
+
+console.log(`Variable one value is "${variable1}" and it's type is "${typeof variable1}"`)
+console.log(`Variable one value is  "${variable2}" and it's type is "${typeof variable2}"`)
+console.log(`Variable one value is  "${variable3}" and it's type is "${typeof variable3}"`)
+
+
+/* 
+Variable one value is "learnCodeOnline" and it's type is "string"
+Variable one value is  "undefined" and it's type is "undefined"
+Variable one value is  "null" and it's type is "object"
+*/
