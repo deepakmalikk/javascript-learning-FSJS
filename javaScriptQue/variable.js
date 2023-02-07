5206fb699c30c396afc75767cfafa27ab8155fd5
deepakmalikk <deepak164malik@gmail.com>
2023-02-07 13:32:37 +0530

    solved 8th problem

--- javaScriptQue/variable.js
+++ javaScriptQue/variable.js
@@ -122,3 +122,19 @@ console.log(occurrence)
 
 // 22
 
+
+
+
+// 8. Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'.
+
+let sentence = 'You cannot end a sentence with because because because is a conjunction'
+let pos = sentence.search('because');
+console.log(pos)
+
+// 31
+
+
+
+
+
+
