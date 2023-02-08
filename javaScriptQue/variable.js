0b06a56152273608ef9eb361d9df60c83e2d87e5
deepakmalikk <deepak164malik@gmail.com>
2023-02-08 13:38:19 +0530

    solved 12th problem

--- javaScriptQue/variable.js
+++ javaScriptQue/variable.js
@@ -240,10 +240,40 @@ console.log(py.length > jar.length)
 // false
 
 
+
+
+
 /*
+12. Use the Date object to do the following activities
+    - What is the year today?
+    - What is the month today as a number?
+    - What is the date today?
+    - What is the day today as a number?
+    - What is the hours now?
+    - What is the minutes now?
+    - Find out the numbers of seconds elapsed from January 1, 1970 to now.
 
 */
 
+let date = new Date();
 
+console.log(date.getFullYear())
+// 2023
 
+console.log(date.getMonth()) // month starts from 0-11
+// 1 
 
+console.log(date.getDate())
+// 8
+
+console.log(date.getDay())
+// 3  //wedneday
+
+console.log(date.getHours())
+// 13
+
+console.log(date.getMinutes())
+// 23
+
+console.log(Date.now())
+// 1675843599458   // seconds from 1 jan 1970 till 08 feb 2023
