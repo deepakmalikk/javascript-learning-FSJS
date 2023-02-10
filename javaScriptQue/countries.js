d9169e06e20ceabb04c9547d69d32cf7768c789f
deepakmalikk <deepak164malik@gmail.com>
2023-02-10 19:15:19 +0530

    on hold problem 24

---
+++ javaScriptQue/countries.js
@@ -0,0 +1,10 @@
+// 24. Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+
+// export let countries = ['India', 'china', 'pakistan', 'nepal', 'bangladash', 'australia', 'new zealand', 'america', 'UK']
+
+// export default countries
+
+
+
+
+
---
+++ javaScriptQue/web_techs.js
@@ -0,0 +1,3 @@
+// 24. create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+
+let webTechs =['html', 'css', 'js', 'nodejs', 'express', 'bootstarp', 'tailwind', 'react', 'mongodb', 'Mysql']
