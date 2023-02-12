a93097539a90551bff762573dffadcbb61f6f636
deepakmalikk <deepak164malik@gmail.com>
2023-02-12 18:18:00 +0530

    solved 33th problem

--- javaScriptQue/main.js
+++ javaScriptQue/main.js
@@ -559,3 +559,34 @@ for(let i of countryList){
     reversedarr.push(capital)
 }
 console.log(reversedarr)
+
+
+
+
+/*
+33. Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
+    - The same groups apply to both men and women.
+    - Underweight: BMI is less than 18.5
+    - Normal weight: BMI is 18.5 to 24.9
+    - Overweight: BMI is 25 to 29.9
+    - Obese: BMI is 30 or more
+
+*/
+
+let w =80 ;
+let h = 0.5678
+
+let bmi = w*(h*h)
+if(bmi<=18.5){
+    console.log("Underweight")
+}
+else if(bmi>18.5 && bmi<=24.9){
+    console.log('Normal');
+}
+else if(bmi >=25 && bmi < 29.9){
+    console.log('Overweight')
+}
+else{
+    console.log('obese')
+}
+// Overweight
