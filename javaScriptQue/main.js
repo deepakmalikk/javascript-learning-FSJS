27a0e3cdedfabc2624fdd4f0030e6d3ebd50334b
deepakmalikk <deepak164malik@gmail.com>
2023-02-12 18:22:18 +0530

    left 34th problem

--- javaScriptQue/main.js
+++ javaScriptQue/main.js
@@ -590,3 +590,8 @@ else{
     console.log('obese')
 }
 // Overweight
+
+
+
+
+// 34. Linear equation is calculated as follows: ax + by + c = 0. Write a function which calculates value of a linear equation, solveLinEquation.
